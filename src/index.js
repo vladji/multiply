f3a33a2cf98a45ef1ead9512f59fa3e9a3ae0fad
vladji <vladjislav@gmail.com>
2019-10-01 19:06:54 +0300

    feat: multiply is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let result = [];
+  const firstArr = first.split('').reverse();
+  const secondArr = second.split('').reverse();
+
+  for (let i = 0; i < firstArr.length; i++) {
+    for (let k = 0; k < secondArr.length; k++) {
+      const mulNum = firstArr[i] * secondArr[k];
+      result[i + k] = result[i + k] ? result[i + k] + mulNum : mulNum;
+    }
+  }
+
+  for (let i = 0; i < result.length; i++) {
+    const num = result[i] % 10;
+    const rest = Math.floor(result[i] / 10);
+    result[i] = num;
+
+    if (rest > 0) {
+      result[i + 1] = result[i + 1] ? result[i + 1] += rest : rest;
+    }
+  }
+  return result.reverse().join('');
 }
